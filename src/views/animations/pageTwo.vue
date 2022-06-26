422cccef4a29218a3046e436ca165fcdadff1258
kromate <akpananthony33@gmail.com>
2022-06-26 23:00:00 +0100

    feat: second animation

--- src/views/animations/pageTwo.vue
+++ src/views/animations/pageTwo.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="body">
+	<div class="body px-5">
 		<p>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
 			possimus pariatur omnis voluptatibus aspernatur, minus labore adipisci,
@@ -11,12 +11,16 @@
 			dignissimos illum ducimus!
 		</p>
 
-		<p id="second-p">
+		<p class="mt-3">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
 			possimus pariatur omnis voluptatibus aspernatur, minus labore adipisci,
 			consequatur excepturi aliquam voluptatem eligendi nisi fugiat. Corporis
 			vero necessitatibus dignissimos illum ducimus! Lorem ipsum dolor sit amet
 			consectetur adipisicing elit.
+		</p>
+
+		<p @click="replay" class="bg-black px-9 py-2 !text-white mt-5 cursor-pointer">
+			Replay
 		</p>
 
 		<div class="welcome">
@@ -30,11 +34,12 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { gsap } from 'gsap';
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
 
-const timeline = gsap.timeline({ ease: 'Expo.easeOutIn', duration: 1.8 });
+const timeline = gsap.timeline({ ease: 'Expo.easeOutIn', duration: 1.8, delay:0 })
 onMounted(() => {
+
 	timeline.fromTo(
 		'.welcome',
 		{
@@ -46,22 +51,23 @@ onMounted(() => {
 			opacity: 1,
 			duration: 0.8,
 		}
-	);
+	)
 
-	timeline.fromTo('.heading', { y: '-30px' }, { y: 0, opacity: 1 });
-	timeline.fromTo('.sub-heading', { y: '-30px' }, { y: 0, opacity: 1 });
-	timeline.to('.loader-wrapper', { opacity: 1 });
-	timeline.fromTo('.loader', { width: '0%' }, { width: '100%', duration: 2.3 });
-	timeline.to('.welcome', { x: '100vw' });
-	timeline.fromTo('p', { y: '-30px' }, { y: 0, opacity: 1 });
-});
+	timeline.fromTo('.heading', { y: '-30px' }, { y: 0, opacity: 1 })
+	timeline.fromTo('.sub-heading', { y: '-30px' }, { y: 0, opacity: 1 })
+	timeline.to('.loader-wrapper', { opacity: 1 })
+	timeline.fromTo('.loader', { width: '0%' }, { width: '100%', duration: 2.3 })
+	timeline.to('.welcome', { x: '100vw', display: 'none' })
+	timeline.fromTo('p', { y: '-30px' }, { y: 0, opacity: 1, stagger: 0.35 })
+})
+
+const replay = () => timeline.seek(0)
 </script>
 
 <style scoped>
 .body {
 	height: 100vh;
 	width: 100vw;
-	padding: 0;
 	margin: 0;
 	display: flex;
 	align-items: center;
