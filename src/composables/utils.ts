f04303e18e29ef4b922b62cb34d0688794678621
kromate <akpananthony33@gmail.com>
2022-06-26 22:07:23 +0100

    added to the list

--- src/composables/utils.ts
+++ src/composables/utils.ts
@@ -4,5 +4,11 @@ export const animationPageList = [
 		title: 'Animation One',
 		desc: 'This is the same as the website intro, Just with a reverse button added',
 		link: '/animations/pageOne',
+	},
+	{
+		index: 2,
+		title: 'Animation Two',
+		desc: 'lorem',
+		link: '/animations/pageTwo',
 	}
 ]
