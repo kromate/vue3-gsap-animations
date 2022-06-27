e207c5d2b618df8a84956de3ff26fed1de0023ee
kromate <akpananthony33@gmail.com>
2022-06-28 00:00:01 +0100

    feat: update

--- src/views/animations/pageThree.vue
+++ src/views/animations/pageThree.vue
@@ -1,6 +1,14 @@
 <template>
-	<div class="text-xl">
-		3
+	<div class="container">
+		<p class="card-name">Platinum Card</p>
+		<h1 class="card-balance">$332,176.66</h1>
+		<img class="logo" src="images/ring.png" alt="">
+		<img class="sim" src="images/sim.png" alt="">
+
+		<p class="card-number">**** **** 3322</p>
+		<p class="due-date">07/25</p>
+
+		<img src="images/pattern.png" class="card-logo" alt="">
 	</div>
 </template>
 
