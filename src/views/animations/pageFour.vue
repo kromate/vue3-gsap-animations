c1feb86eb26943058de29c772f11d23505c3f705
kromate <akpananthony33@gmail.com>
2022-06-27 23:59:39 +0100

    feat: update

--- src/views/animations/pageFour.vue
+++ src/views/animations/pageFour.vue
@@ -1,6 +1,77 @@
 <template>
-	<div>
-		444
+	<div class="wrapper">
+		<div class="nav">
+			<div class="logo">Clean Juice</div>
+
+			<div class="menu-links">
+				<ul>
+					<li>locations</li>
+					<li>our menu</li>
+					<li>our story</li>
+					<li>meet our teem</li>
+					<li>own a clean juice</li>
+					<li>contact us</li>
+				</ul>
+			</div>
+
+			<div class="search"><i class="fa fa-search"></i></div>
+
+			<div class="account">my account</div>
+
+			<div class="cart"><i class="i fa fa-shopping-cart"></i></div>
+		</div>
+
+		<div class="content">
+			<div class="tagline">Find your clean juice</div>
+
+			<div class="pages"><span>2</span>/21</div>
+
+			<div class="title">orange</div>
+
+			<div class="more"><a href="#">show all the juices</a></div>
+
+			<div class="desc">
+				<p>
+					Your <span>healthy</span> life <br>
+					starts here with us
+				</p>
+				<p>
+					A family owned company founded with the purpose of giving your family access to clean, organic products while
+					you are on the go.
+				</p>
+			</div>
+		</div>
+
+		<div class="juice">
+			<img src="./img/juice.png" alt="">
+		</div>
+
+		<div class="leaves">
+			<ul id="scene">
+				<li class="layer" data-depth="-.1">
+					<img src="./img/leaf01.png" alt="">
+				</li>
+				<li class="layer" data-depth="-.3">
+					<img src="./img/leaf02.png" alt="">
+				</li>
+				<li class="layer" data-depth="-1.5">
+					<img src="./img/leaf03.png" alt="">
+				</li>
+				<li class="layer" data-depth=".1">
+					<img src="./img/leaf04.png" alt="">
+				</li>
+				<li class="layer" data-depth=".3">
+					<img src="./img/leaf05.png" alt="">
+				</li>
+			</ul>
+		</div>
+
+		<div class="arrows">
+			<button class="prev"><i class="fa fa-chevron-left"></i></button>
+			<button class="next"><i class="fa fa-chevron-right"></i></button>
+		</div>
+
+
 	</div>
 </template>
 
