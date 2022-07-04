271e5c8686cd5f522e60aee13fc979c976e14814
kromate <akpananthony33@gmail.com>
2022-07-04 19:02:02 +0100

    update

--- src/views/animations/pageZero.vue
+++ src/views/animations/pageZero.vue
@@ -1,24 +1,25 @@
 <template>
 	<div class="center h-screen w-screen bg">
-		<div class="relative center text-white pos">
+		<div class="line bg-white absolute h-0.5 w-96 z-10"/>
+		<div class="relative center text-white pos text-7xl">
+		
 			<img
 				src="../../assets/images/pageZero/border.svg"
 				alt="border"
 				class="logo-border absolute w-40 rotate-45"
 			/>
 
-			<span class="text-7xl p-10 font-semibold">K</span>
+			<span class=" p-10 font-semibold">K</span>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-for(let i= 0; i<=100; i++){
-	console.log(`${i}% {background-image: linear-gradient(90deg, #000000 ${i}%, #958e69 0%);}`)
-}
+
 </script>
 
 <style scoped>
+
 .bg {
 	   background: #958e69;
      transition: 0.8s ease-out;
@@ -39,26 +40,24 @@ for(let i= 0; i<=100; i++){
 	animation: spin 1.5s 1;
 
 }
+.pos{
+    animation: fade 0.5s 1.5s 1 forwards;
+}
 
+@keyframes fade {
+    from{opacity:1;}
+    to{ opacity:0; display: none;}
+}
 
 @keyframes flow {
-    from{
-          right: 100%;
-    }
-    to{
-        right: 0
-    }
+    from{right: 100%;}
+    to{right: 0;}
 }
+
 @keyframes spin {
-	0% {
-		transform: rotate(45deg) scale(1);
-	}
-    50%{
-        transform: rotate(135deg) scale(1.2);
-    }
-    100%{
-        transform: rotate(225deg) scale(1);
-    }
+	0% {transform: rotate(45deg) scale(1);}
+    50%{transform: rotate(135deg) scale(1.5);}
+    100%{transform: rotate(225deg) scale(1); }
 }
 </style>
 
