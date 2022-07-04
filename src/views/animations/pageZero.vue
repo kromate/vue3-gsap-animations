0246c158bf1ff296917ceb99e73930e04d5e36fc
kromate <akpananthony33@gmail.com>
2022-07-04 19:45:22 +0100

    feat: done

--- src/views/animations/pageZero.vue
+++ src/views/animations/pageZero.vue
@@ -1,25 +1,36 @@
 <template>
-	<div class="center h-screen w-screen bg">
-		<div class="line bg-white absolute h-0.5 w-96 z-10"/>
-		<div class="relative center text-white pos text-7xl">
+	<div class="h-[200vh]">
+		<div class="center h-screen w-full bg shadow-md">
+			<!-- <div class="line bg-[#ffffff7b] absolute h-0.5 w-full z-10 top-12"/> -->
+			<!-- <div class="line bg-[#ffffff7b] absolute h-0.5 w-full z-10 bottom-12"/> -->
+			<div class="line bg-[#ffffff7b] fixed h-0 w-0.5 z-10 md:left-16 left-5"/>
+			<div class="line bg-[#ffffff7b] fixed h-0 w-0.5 z-10 md:right-16 right-5"/>
+			<div class="relative center text-white pos text-7xl">
 		
-			<img
-				src="../../assets/images/pageZero/border.svg"
-				alt="border"
-				class="logo-border absolute w-40 rotate-45"
-			/>
+				<img
+					src="../../assets/images/pageZero/border.svg"
+					alt="border"
+					class="logo-border absolute w-40 rotate-45"
+				/>
 
-			<span class=" p-10 font-semibold">K</span>
+				<span class=" p-10 font-semibold">K</span>
+			</div>
 		</div>
 	</div>
+
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-
+<style >
+body{
+    background: #212143;
+}
+.line{
+    animation: grow 1.5s 1.5s 1 forwards;
+}
 .bg {
 	   background: #958e69;
      transition: 0.8s ease-out;
@@ -27,7 +38,7 @@
 .bg:before {
      content: "";
      position: absolute;
-     background: #000;
+     background: #212143;
      bottom: 0;
      left: 0;
      right: 100%;
@@ -44,9 +55,13 @@
     animation: fade 0.5s 1.5s 1 forwards;
 }
 
+@keyframes grow {
+    from{opacity:0; height: 0;}
+    to{ opacity:1; height: 100%;}
+}
 @keyframes fade {
     from{opacity:1;}
-    to{ opacity:0; display: none;}
+    to{ opacity:0; display: none; transform: scale(0.2);}
 }
 
 @keyframes flow {
