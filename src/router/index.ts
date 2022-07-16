13780c0af9681c25e1c942b1fd51658f599af5e1
kromate <akpananthony33@gmail.com>
2022-07-16 22:36:13 +0100

    feat: adding custom animated 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 // @ts-ignore
-import routes from '~pages';
+import routes from '~pages'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    ...routes,
-    {
-      path: '/:catchAll(.*)',
-      component: () => import('../views/index.vue'),
-    },
-  ],
-});
+	history: createWebHistory(),
+	routes: [
+		...routes,
+		{
+			path: '/:catchAll(.*)',
+			component: () => import('../views/index.vue'),
+		},
+	],
+})
 
 // router.afterEach(() => {
 // 	window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
 // })
 
-export { router };
+export { router }
---
+++ src/views/animations/404.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		404
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
